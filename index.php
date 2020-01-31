5efa96e1177cb9c4fefe6432418aa74d08db2c69
Berna <Bernardo-A-Roque>
2020-01-30 22:07:17 -0400

    I dont care

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html5>
+<html>
+	<head>
+		<meta charset="UTF-8" name="keywords" content="Programming, Languages, Bernardo A Roque" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"-->
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>	
+	<body>
+		<header>
+			<ul>
+				<li><a href="Python.html">Python</a></li>
+				<li><a href="Rust.html">Rust</a></li>
+				<li><a href="Java.html">Java </a></li>
+				<li><a href="CPP.html">C++ </a></li>
+				<li><a href="JS.html">Javascript </a></li>
+				<li><a href="PHP.html">PHP </a></li>
+				<li><a href="Bash.html">Bash </a></li>
+				<li><a href="Kotlin.html">Kotlin </a></li>
+				<li><a href="Csharp.html">C# </a></li>
+				<li><a href="Swift.html">Swift</a></li>
+			</ul>
+		</header>
+		<?php print("Hello World"); ?>
+	</body>
+</html>
